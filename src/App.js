5f2a1e1760ee79a647d3c13fdae8cf5f706d8772
sem1on <sgorlov21@gmail.com>
2022-03-28 19:14:04 +0300

    добавление некоторых компонентов

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 
-import './App.css';
+import PostList from './Components/PostList';
+
+import './Styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const [posts, setPost] = useState([
+        {id: 1, title: 'JS', body: 'Description'},
+        {id: 2, title: 'PT', body: 'Description'},
+        {id: 3, title: 'C#', body: 'Description'},
+    ])
+
+    return (
+        <div className="App">
+            <PostList posts={posts} title={'Список постов 1'}/>
+        </div>
+    );
 }
 
 export default App;
