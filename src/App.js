cd6a6211d69dfb2e882940fbf34aee14a9f30db2
sem1on <sgorlov21@gmail.com>
2022-03-30 19:32:26 +0300

    Добавлен функционал по добавлению новых постов

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
 import { useState } from 'react';
 
 import PostList from './Components/PostList';
+import MyButton from './Components/UI/button/MyButton';
+import MyInput from './Components/UI/input/MyInput';
 
 import './Styles/App.css';
 
 function App() {
 
-    const [posts, setPost] = useState([
+    const [posts, setPosts] = useState([
         {id: 1, title: 'JS', body: 'Description'},
         {id: 2, title: 'PT', body: 'Description'},
         {id: 3, title: 'C#', body: 'Description'},
     ])
+    const [post, setPost] = useState({title: '', body: ''})
+
+    const addNewPost = (e) => {
+        e.preventDefault();
+        setPosts([...posts, {...post, id: Date.now()}]);
+        setPost({title: '', body: ''});
+    };
 
     return (
         <div className="App">
-            <PostList posts={posts} title={'Список постов 1'}/>
+            <form>
+                <MyInput 
+                    type="text" 
+                    placeholder='Название поста'
+                    value={post.title}
+                    onChange={e => setPost({...post, title: e.target.value})}
+                />
+                <MyInput 
+                    value={post.body}
+                    onChange={e => setPost({...post, body: e.target.value})}
+                    type="text" 
+                    placeholder='Описание'
+                />
+                <MyButton onClick={addNewPost}>Создать пост</MyButton>
+            </form>
+            <PostList posts={posts} title={'Список постов'}/>
         </div>
     );
 }
