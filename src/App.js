d75669661e7d0748e69c0523f5d3a3be347523e0
sem1on <sgorlov21@gmail.com>
2022-03-31 18:27:00 +0300

    добавленна сортировка по названию или описанию

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import PostList from './Components/PostList';
 import PostForm from './Components/PostForm';
+import MySelect from './Components/UI/select/MySelect';
 
 
 import './Styles/App.css';
@@ -14,6 +15,8 @@ function App() {
         {id: 3, title: 'C#', body: 'Description'},
     ])
 
+    const [selectedSort, setSelectedSort] = useState('');
+
     const onAdd = (newPost) => {
         setPosts([...posts, newPost])
     };
@@ -22,14 +25,30 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
+    const sortPost = (sort) => {
+        setSelectedSort(sort);
+        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+    }
+
     return (
         <div className="App">
             <PostForm onAdd={onAdd}/>
-            <PostList 
-                posts={posts} 
-                title={'Список постов'}
-                onRemove={onRemove}
+            <hr style={{margin: '15px 0'}} />
+            <div>
+                <MySelect 
+                    value={selectedSort}
+                    onChange={sortPost}
+                    defaultValue='Сортировка'
+                    options={[
+                        {value: 'title', name: 'По названию'},
+                        {value: 'body', name: 'По описанию'}
+                    ]}
                 />
+            </div>
+            {posts.length
+                ? <PostList  posts={posts}  title={'Список постов'} onRemove={onRemove}/>
+                : <h1 style={{textAlign: 'center'}}> Посты не найдены</h1>
+            }
         </div>
     );
 }
