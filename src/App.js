d74b7411b3a2f0c556176f7d9d8209c9b28fd06d
sem1on <sgorlov21@gmail.com>
2022-03-31 23:06:01 +0300

    создан собственный хук с функционалоп по поиску и сортировки постов

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
+
+import { usePosts } from './hooks/usePost';
 
 import PostList from './Components/PostList';
 import PostForm from './Components/PostForm';
 import PostFilter from './Components/PostFilter';
 
-import './Styles/App.css';
 import MyModal from './Components/UI/modal/MyModal';
 import MyButton from './Components/UI/button/MyButton';
 
+import './Styles/App.css';
 
 
 function App() {
@@ -21,17 +23,8 @@ function App() {
     const [filter, setFilter] = useState({sort: '', query: ''});
     const [modal, setModal] = useState(false);
 
-    const sortedPosts = useMemo(() => {
-        if(filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts]);
-
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+    
     const onAdd = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
